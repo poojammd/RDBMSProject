aa186fa82960da4a11d45a0b0912c73952b3db4c
Pooja Muraleedharan <pooja.mmd@gmail.com>
2018-10-25 00:40:30 +0530

    Last one on 25th Oct

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+create database travel
+use travel
+
+create table users(username varchar(10) PRIMARY KEY,passwd varchar(10))
+select * from users
+insert into users values ('poop','123')
+
+drop table users
